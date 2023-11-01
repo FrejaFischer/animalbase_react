88be7a36fcfdc573f6a9460acd2f5b252774d011
Freja Fischer <frej657e@stud.kea.dk>
2023-11-01 10:24:09 +0100

    filtering and sorting works

--- src/App.jsx
+++ src/App.jsx
@@ -6,46 +6,96 @@ import "./App.css";
 function App() {
   const animals = getData();
   const [filterProp, setFilterProp] = useState("");
+  const [sortColumn, setSortColumn] = useState("name");
+  const [sortDirection, setSortDirection] = useState("asc");
   const speciesTypes = [];
+
+  animals.map((spec) => {
+    if (!speciesTypes.includes(spec.species)) {
+      speciesTypes.push(spec.species);
+    }
+  });
   let filtered = animals;
   if (filterProp) {
     filtered = animals.filter((ani) => ani.species === filterProp);
   }
+
+  function sortAnimals(a, b) {
+    if (a[sortColumn] > b[sortColumn]) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+  filtered.sort(sortAnimals);
+  if (sortDirection === "desc") {
+    filtered.reverse();
+  }
   return (
     <div>
-      {/* <button onClick={() => setFilterProp("")}>Show all</button>
-      <button onClick={() => setFilterProp("cat")}>Only Cats</button>
-      <button onClick={() => setFilterProp("dog")}>Only Dogs</button>
-      <button onClick={() => setFilterProp("horse")}>Only Horses</button>
-      <button onClick={() => setFilterProp("dragon")}>Only Dragons</button> */}
-      {animals.map(
-        (spec) => {
-          if (!speciesTypes.includes(spec.species)) {
-            console.log("new item added");
-            // spec.species = [...speciesTypes];
-            // !speciesTypes.some((item) => item.includes(spec.species))
-            speciesTypes.push(spec.species);
-          }
-          console.log(speciesTypes);
-        }
-        // <button onClick={() => setFilterProp(`${spec.species}`)}>Only {spec.species}s</button>
-      )}
+      <button onClick={() => setFilterProp("")}>Show all</button>
+
+      {speciesTypes.map((specType) => (
+        <button key={specType} onClick={() => setFilterProp(`${specType}`)}>
+          Only {specType}s
+        </button>
+      ))}
       <table>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Trait</th>
-            <th>Species</th>
-            <th>Age</th>
+            <th>
+              {" "}
+              <button
+                onClick={() => {
+                  setSortColumn("name");
+                  setSortDirection((prevSortDirection) => (prevSortDirection === "asc" ? "desc" : "asc"));
+                }}
+              >
+                Name
+              </button>
+            </th>
+            <th>
+              {" "}
+              <button
+                onClick={() => {
+                  setSortColumn("trait");
+                  setSortDirection((prevSortDirection) => (prevSortDirection === "asc" ? "desc" : "asc"));
+                }}
+              >
+                Trait
+              </button>
+            </th>
+            <th>
+              {" "}
+              <button
+                onClick={() => {
+                  setSortColumn("species");
+                  setSortDirection((prevSortDirection) => (prevSortDirection === "asc" ? "desc" : "asc"));
+                }}
+              >
+                Species
+              </button>
+            </th>
+            <th>
+              {" "}
+              <button
+                onClick={() => {
+                  setSortColumn("age");
+                  setSortDirection((prevSortDirection) => (prevSortDirection === "asc" ? "desc" : "asc"));
+                }}
+              >
+                Age
+              </button>
+            </th>
           </tr>
         </thead>
         <tbody>
           {filtered.map((ani) => (
-            <tr>
-              <td>{ani.name}</td>
-              <td>{ani.trait}</td>
-              <td>{ani.species}</td>
-              <td>{ani.age}</td>
+            <tr key={ani.name + "1"}>
+              <td key={ani.name}>{ani.name}</td>
+              <td key={ani.trait}>{ani.trait}</td>
+              <td key={ani.species}>{ani.species}</td>
+              <td key={ani.age}>{ani.age}</td>
             </tr>
           ))}
         </tbody>
